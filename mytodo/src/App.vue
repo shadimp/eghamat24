c2d6617afb62a2593e591c6397f55717aea7bf8c
shadi <shd.yahoo.com>
2022-08-21 12:55:01 +0430

    datepicker

---
+++ mytodo/src/App.vue
@@ -0,0 +1,75 @@
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class='ui centered card'>
+    // Todo shown when we are not in editing mode.
+    <div class="content" v-show="!isEditing">
+      <div class='header'>
+          {{ todo.title }}
+      </div>
+      <div class='meta'>
+          {{ todo.project }}
+      </div>
+      <div class='extra content'>
+          <span class='right floated edit icon' v-on:click="showForm">
+          <i class='edit icon'></i>
+        </span>
+      </div>
+    </div>
+    // form is visible when we are in editing mode
+    <div class="content" v-show="isEditing">
+      <div class='ui form'>
+        <div class='field'>
+          <label>Title</label>
+          <input type='text' v-model="todo.title" >
+        </div>
+        <div class='field'>
+          <label>Project</label>
+          <input type='text' v-model="todo.project" >
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
+        Completed
+    </div>
+    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+        Pending
+    </div>
+  </div>
+</template>
---
+++ mytodo/src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li> خرید از فروشگاه اینترنتی آفر</li>
+            <li>مطالعه مقاله های لاراول در روکسو</li>
+            <li>دانلود آهنگ از وب سایت ادبی هنری پرسه</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+
+    export default{}
+
+</script>
